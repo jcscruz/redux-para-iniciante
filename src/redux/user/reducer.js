79aaad799fd863345a09c341bd28db57e77048bc
julio.cruz <julio.cruz@g.globo>
2024-05-28 16:18:04 -0300

    Criado o primeiro reducer

--- src/redux/user/reducer.js
+++ src/redux/user/reducer.js
@@ -1,3 +1,15 @@
-const userReducer = () => {}
+const initialState = {
+    currentUser: null,
+}
+
+const userReducer = (state = initialState, action) => {
+    if(action.type === 'user/login'){
+        return {
+            ...state, currentUser: 10
+        }        
+    }
+
+    return state
+}
 
 export default userReducer
